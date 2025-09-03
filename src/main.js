9ce9b66447fcb2a5f3dadf0f051eaccf5d98827b
Takashi Ota <gmkzkks1011@gmail.com>
2025-09-03 17:57:38 +0900

    角度によって数字を配置することに成功　次は太短や細長の配置

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import './styles/main.scss';
 
+
+/* ----- 
+針を動かすコード
+----- */
 const tickClock = () => {
   /* 現在の時分秒を取得 */
   const time = new Date();
@@ -12,8 +16,38 @@ const tickClock = () => {
   const minDeg = min * 6 + (sec / 60) * 6;
   const secDeg = sec * 6
 
+  /* 求めた角度をtransformプロパティの値にあててる */
   document.querySelector('#hour').style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
   document.querySelector('#min').style.transform = `translateX(-50%) rotate(${minDeg}deg)`;
   document.querySelector('#sec').style.transform = `translateX(-50%) rotate(${secDeg}deg)`;
 };
-setInterval(tickClock, 100);
+
+setInterval(tickClock, 100); // 0.1秒ごとに表示更新
+
+
+
+
+/* ----- 
+文字盤の数字と目盛りを生成するコード
+----- */
+const clock = document.querySelector('.clock'); //時計を取得
+const test = () => {
+  for (let i = 1; i <= 60; i++) {
+    const tick = document.createElement('span'); // spanを60個生成
+
+    if (i % 15 === 0) { // 15の倍数の時
+      tick.classList.add('clock__number');
+      tick.textContent = (i / 5) === 12 ? 12 : i / 5;
+    } else if (i % 5 === 0) { // 5の倍数の時
+      tick.classList.add('clock__major-tick');
+    } else { // 上記以外の時
+      tick.classList.add('clock__minor-tick');
+    }
+
+    const degree = i * 6;
+
+    tick.style.transform = `translate(-50%, -50%) rotate(${degree}deg) translateY(-230px) rotate(-${degree}deg)`;
+    clock.appendChild(tick);
+  }
+}
+test();
